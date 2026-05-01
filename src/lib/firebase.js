a2a80bf9c71b9f7772b21255ca8310e1035d714e
bye315 <omerosmanaplak50@gmail.com>
2026-05-01 22:07:49 +0300

    Update firebase config to use environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { initializeFirestore, memoryLocalCache } from "firebase/firestore"; 
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = { 
-  apiKey: "AIzaSyDUW0-976dmK-0DpmoUhZxQru0mWfady5Y", 
-  authDomain: "test-b46ab.firebaseapp.com", 
-  projectId: "test-b46ab", 
-  storageBucket: "test-b46ab.firebasestorage.app", 
-  messagingSenderId: "941602146363", 
-  appId: "1:941602146363:web:e26512b29285285278341e", 
-  measurementId: "G-RKJ3RFHBL1" 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDUW0-976dmK-0DpmoUhZxQru0mWfady5Y",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "test-b46ab.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "test-b46ab",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "test-b46ab.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "941602146363",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:941602146363:web:e26512b29285285278341e",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-RKJ3RFHBL1"
 }; 
 
 // Initialize Firebase
